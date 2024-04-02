4007215ac14951872acb4bdc0b3cb744e87c5ef9
André Kunde <andrelbkunde@gmail.com>
2024-04-02 09:24:14 -0300

    some components and screens

---
+++ src/app/register.tsx
@@ -0,0 +1,50 @@
+import { Image, StatusBar, View } from "react-native";
+import { Link } from "expo-router";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
+
+import { colors } from "@/styles/colors";
+
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+
+export default function Register() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+
+      <View className="w-full mt-8 gap-3">
+        <Input>
+          <FontAwesome6
+            name="user-circle"
+            size={20}
+            color={colors.green[200]}
+          />
+
+          <Input.Field placeholder="Nome completo" autoCapitalize="words" autoCorrect={false} />
+        </Input>
+
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color={colors.green[200]}
+          />
+
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+        </Input>
+
+        <Button title="Realizar inscrição" />
+
+        <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">
+          Já possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
